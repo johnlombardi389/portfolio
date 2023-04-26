28a55df2dad4f0dbcd4cbe9a155078810a2d503e
johnlombardi389 <lombardij38@gmail.com>
2023-04-26 14:47:26 -0400

    Added styling to nav through styled-components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,9 +7,12 @@ import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 // Image
 import portLogo from "../assets/images/portLogo.svg";
 
+// Style
+import styled from "styled-components";
+
 const Nav = () => {
   return (
-    <nav>
+    <StyledNav>
       <h1>
         <NavLink id="logo" to={"/portfolio"}>
           <img src={portLogo} alt="John Lombardi Logo" />
@@ -41,8 +44,68 @@ const Nav = () => {
           </a>
         </li>
       </ul>
-    </nav>
+    </StyledNav>
   );
 };
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 5rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background: blue;
+
+  h1 {
+    img {
+      width: 3rem;
+      height: 3rem;
+    }
+  }
+
+  a {
+    cursor: pointer;
+    color: green;
+    text-decoration: none;
+    transition: all 0.3s ease;
+    &:hover {
+      color: purple;
+    }
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+
+    li {
+      padding-left: 3.5rem;
+      position: relative;
+      font-size: 1.1rem;
+
+      svg {
+        width: 1.75rem;
+        height: 1.75rem;
+      }
+    }
+
+    @media (max-width: 770px) {
+      flex-direction: column;
+      ul {
+        li {
+          padding: 0 0.75rem;
+        }
+      }
+    }
+  }
+
+  .active {
+    color: red;
+  }
+`;
 
 export default Nav;
