6e26cc32870ab63aeb5a33e597162c5363d5b239
johnlombardi389 <lombardij38@gmail.com>
2023-04-26 15:21:38 -0400

    Added project data to send to projects component

---
+++ src/data.jsx
@@ -0,0 +1,54 @@
+// Videos
+
+const projectData = () => [
+  {
+    id: 1,
+    title: "Game Pac",
+    tools: ["React", "Redux", "JavaScript", "Sass"],
+    demo: "https://johnlombardi389.github.io/game-tracker/",
+    github: "https://github.com/johnlombardi389/game-tracker",
+    video: gamePacVid,
+  },
+  {
+    id: 2,
+    title: "Music Player",
+    tools: ["React", "JavaScript", "Sass", "Git"],
+    demo: "https://johnlombardi389.github.io/music-player/",
+    github: "https://github.com/johnlombardi389/music-player",
+    video: musicPlayerVid,
+  },
+  {
+    id: 3,
+    title: "Pixel Sketch",
+    tools: ["JavaScript", "CSS", "HTML", "Figma"],
+    demo: "https://johnlombardi389.github.io/pixel-sketch/",
+    github: "https://github.com/johnlombardi389/pixel-sketch",
+    video: pixelSketchVid,
+  },
+  {
+    id: 4,
+    title: "Layout Generator",
+    tools: ["JavaScript", "CSS", "HTML", "Git"],
+    demo: "https://johnlombardi389.github.io/layout-generator/",
+    github: "https://github.com/johnlombardi389/layout-generator",
+    video: layoutGenVid,
+  },
+  {
+    id: 5,
+    title: "Portfolio Website",
+    tools: ["React", "JavaScript", "Sass", "Vite"],
+    demo: "https://johnlombardi389.github.io/portfolio/",
+    github: "https://github.com/johnlombardi389/portfolio",
+    video: portVid,
+  },
+  {
+    id: 6,
+    title: "RPS Game",
+    tools: ["JavaScript", "CSS", "HTML", "Git"],
+    demo: "https://johnlombardi389.github.io/rock-paper-scissors/",
+    github: "https://github.com/johnlombardi389/rock-paper-scissors",
+    video: rpsVid,
+  },
+];
+
+export default projectData;
