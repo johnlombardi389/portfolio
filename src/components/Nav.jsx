7ef9ebb9b9e9f95190607aee72916d4cb041e98d
johnlombardi389 <lombardij38@gmail.com>
2023-04-26 14:33:24 -0400

    Added initial code for each component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <div>
+      <h1>Hello Nav</h1>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div>
+      <h1>Hello Projects</h1>
+    </div>
+  );
+};
+
+export default Projects;
