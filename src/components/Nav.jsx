8aca068a0d644e4bbb93754b5f354ced82ca40e2
johnlombardi389 <lombardij38@gmail.com>
2023-04-26 14:41:15 -0400

    Added logo image and icons for nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,47 @@
+// Router
+import { NavLink } from "react-router-dom";
+
+// Icons
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+
+// Image
+import portLogo from "../assets/images/portLogo.svg";
+
 const Nav = () => {
   return (
-    <div>
-      <h1>Hello Nav</h1>
-    </div>
+    <nav>
+      <h1>
+        <NavLink id="logo" to={"/portfolio"}>
+          <img src={portLogo} alt="John Lombardi Logo" />
+        </NavLink>
+      </h1>
+      <ul>
+        <li>
+          <NavLink to={"/portfolio/projects"}>Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/portfolio/about"}>About</NavLink>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/johnlombardi389/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/johnlombardi389" target="_blank">
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:johnlombardi389@gmail.com">
+            <FaEnvelope />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
