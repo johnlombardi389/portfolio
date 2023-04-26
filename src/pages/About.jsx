3b9c84db05704e84af258cdc101f21229a7d5d99
johnlombardi389 <lombardij38@gmail.com>
2023-04-26 15:47:51 -0400

    Imported icons from react-icons

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,29 @@ import { motion } from "framer-motion";
 // Images
 import aboutBanner from "../assets/images/aboutBanner.png";
 
+// Icons
+import {
+  SiReact,
+  SiSass,
+  SiJavascript,
+  SiCss3,
+  SiHtml5,
+  SiGit,
+  SiRedux,
+  SiVite,
+  SiBootstrap,
+  SiVisualstudiocode,
+  SiFigma,
+  SiAdobephotoshop,
+  SiAdobeillustrator,
+  SiAdobexd,
+  SiMongodb,
+  SiExpress,
+  SiNodedotjs,
+  SiWebpack,
+  SiGithub,
+} from "react-icons/si";
+
 const About = () => {
   return (
     <div>
