a8ea92b94460419771369925a5646372c6578a74
johnlombardi389 <lombardij38@gmail.com>
2023-04-27 21:06:11 -0400

    Updated git ignore

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio/",
-  server: {
-    mime: {
-      "text/javascript": ["js"],
-    },
-  },
 });
