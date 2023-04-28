4f1c6d855948c274baf3967cea6fd09641266c7f
johnlombardi389 <lombardij38@gmail.com>
2023-04-27 20:38:56 -0400

    Specified MIME type in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/portfolio/",
   plugins: [react()],
+  server: {
+    mime: {
+      "text/javascript": ["js"],
+    },
+  },
 });
